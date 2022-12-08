04912265baebd7866efa7ed4149682ee9a2cda2a
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-08 03:11:45 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
 /**
  * main - Entry point
  *
- * Return: 1
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
