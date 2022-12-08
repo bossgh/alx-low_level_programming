5323c755c31b5185cb42f5eb767be55d4a82f8ae
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-08 00:03:35 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+/*
+ *main - Entry point
+ *return: Always 0 (Success) 
+ */
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
