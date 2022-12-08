99aa7f57fb01e2f5e93c8de451f45f1ac449d4ce
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-08 03:08:44 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
