cada21610691948dde38a03546fbbee38d0452b0
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-09 05:06:13 -0800

    Zero to Ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - returns numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
