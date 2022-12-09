c0a31422471e0fccb92ea75713704873de101bc1
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-09 04:31:32 -0800

    Lower and upper cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,13 @@ int main(void)
 	int ch = 'a';
 	int CH = 'A';
 
-	while(ch < 'z')
+	while(ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 
-	while(CH < 'Z')
+	while(CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
