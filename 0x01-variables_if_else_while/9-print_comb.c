699b4e2ef297e5a7d902500e7bb26458bb82517f
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-09 06:12:02 -0800

    List single digits separated by comma.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+ int main(void)
+{
+    int x;
+
+    for (x = 48; x < 58; x++)
+    {
+	    putchar(x);
+
+	    if (x < 57)
+	    {
+		    putchar(44);
+		    putchar(32);
+	    }
+    }
+    putchar('\n');
+    return (0);
+}
