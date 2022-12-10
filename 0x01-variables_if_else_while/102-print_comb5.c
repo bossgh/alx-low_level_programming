244b26bb2dfae4e1be910c086ac712138fc1ee30
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-10 03:35:15 -0800

    Print combination of numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if ((i == 98) || (j == 99))
+				if ((i != 98) || (j != 99))
 				{
 					putchar(',');
 				}	putchar(' ');
