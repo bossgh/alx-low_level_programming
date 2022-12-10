37bd37625e0a8002ae3b237b5b8165dc9ca0765b
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-10 00:51:59 -0800

    All possible combinations.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digits
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int os = '0';
+	int ts = '0';
+
+	for (ts = '0'; ts <= '9'; ts++)
+	{
+		for (os = '0'; os <='9'; os++)
+		{
+			if (!((os == ts) || (ts > os)))
+			{
+				putchar(ts);
+				putchar(os);
+				if (!(os == '9' && ts =='8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
