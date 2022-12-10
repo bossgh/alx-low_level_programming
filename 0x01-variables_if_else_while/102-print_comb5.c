81ac4cc3ba2a84e1097fdc2a6926a913dea0ebc3
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-10 02:57:27 -0800

    Print combination of numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= 98; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
@@ -20,7 +20,7 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if ((i <= 98) || (j <= 99))
+			if ((i == 98) || (j == 99))
 			{
 				continue;
 			}
