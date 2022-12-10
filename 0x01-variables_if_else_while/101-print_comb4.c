e41d4379a75399bf96c39a93deb0cae21602b4f0
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-10 01:23:43 -0800

    Print possible combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+	i = 0;
+	
+	while (i < 1000)
+	{
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 100;
+		
+		if (j < k && k < l)
+		{
+			putchar(l + '0');
+			putchar(k + '0');
+			putchar(j + '0');
+			if (i< 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
