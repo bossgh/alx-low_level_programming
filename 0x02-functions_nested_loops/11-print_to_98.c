81bcda88a3b063d750fa22dc174ff6a267e809e3
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-13 05:17:21 -0800

    Up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print from input to 98
+ *
+ * @n: the starting number
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			_putchar("%d, ", n--);
+		_putchar("%d\n", n);
+
+	}
+	else
+	{
+		while (n < 98)
+			_putchar("%d, ", n++);
+		_putchar("%d\n", n);
+	}
+}
