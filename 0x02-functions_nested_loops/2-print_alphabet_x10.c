5397791569cd76269e601e392a67b17c198530e5
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-13 00:57:37 -0800

    Print abc ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabets ten times
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet_x10(void)
+{
+	char abc = 'a';
+	int i;
+
+	for (i = 0; i < 10; i ++)
+	{
+		while (abc <= z)
+		{
+			_putchar(abc);
+		}
+	}
+}
