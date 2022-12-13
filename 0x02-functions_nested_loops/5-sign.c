64473a2533c920fe40d7276a5e4d349c05ced21e
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-13 02:57:14 -0800

    Print the sign.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the inpuut
+ *
+ * @n: the input number
+ *
+ * Return: Always 1 for +ve, 0 for zero and -1 for -ve
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
