99e96dd0895a7647679719e82c78812c00cf9989
ADANE WEDAJO <adanewedajo2@gmail.com>
2022-12-15 13:26:14 +0300

    For more numbers.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');		
-
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+			_putchar('\n');
 		}
 }
