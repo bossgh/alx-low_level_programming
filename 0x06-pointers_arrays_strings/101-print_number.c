04f2c3d1cafe38c929c402d7dff36ee68157857f
Adane Wedajo <adanewedajo2@gmail.com>
2022-12-21 09:42:59 -0800

    Commit with correction

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#includde "main.h"
+#include "main.h"
 
 /**
  * print_number - Prints any integer with putchar
@@ -21,5 +21,6 @@ void print_number(int n)
 
 	if (x / 10)
 		print_number(x / 10);
-		_putchar(x % 10 + '0');
+	
+	_putchar(x % 10 + '0');
 }
