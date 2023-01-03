df57164fe3ae4a8aa777d893ee30fa675df345ea
ADANE WEDAJO <adanewedajo2@gmail.com>
2023-01-03 12:27:04 +0300

    Commit for strpbrk.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
 
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
- * @s:first occurrence in the string
+ * @s: first occurrence in the string
  * @accept: matches one of the bytes, or @NULL if no such byte
  *
  * Return: a pointer to the byte
